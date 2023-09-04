d1f08076e8d53735c40c5a52354bf1776f526e76
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-09-04 13:26:43 +0530

    16

--- components/video-grid.tsx
+++ components/video-grid.tsx
@@ -17,8 +17,8 @@ interface videoProps {
 const VideoGrid = ({videos}:{videos : Array<videoProps> | undefined})=>{
 return (
     <div className="flex flex-row-reverse  items-center justify-end  gap-5 flex-wrap w-screen   ">
-    {videos?.map((video : any)=>(
-        <Video video={video}  key={video}  />
+    {videos?.map((video : videoProps)=>(
+        <Video video={video}  key={video.id}  />
     ))}
     </div>
 )
